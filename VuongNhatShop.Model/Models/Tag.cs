23dc46a3e5cfc63c939c6e39d304198fbcb5d1bd
ASUS\PC ASUS <doanthanhminh92@gmail.com>
2017-11-20 21:38:14 +0700

    Bai 5_add domain entity framwork

---
+++ VuongNhatShop.Model/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VuongNhatShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { set; get; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Type { set; get; }
+    }
+}
---
+++ VuongNhatShop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VuongNhatShop.Model.Models
+{
+
+    [Table("VisitorStatistics")]
+    public class VisitorStatistic
+    {
+        [Key]
+        public Guid ID { set; get; }
+
+        [Required]
+        public DateTime VisitedDate { set; get; }
+
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
